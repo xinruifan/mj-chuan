4c3fb110539e68242feefe4d61f202e0ac4772f1
xinrui.fan <xinrui.fan@ly.com>
2025-01-07 11:20:01 +0800

    init

---
+++ src/main/java/com/mj/mjchuan/MjChuanApplication.java
@@ -0,0 +1,13 @@
+package com.mj.mjchuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjChuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjChuanApplication.class, args);
+    }
+
+}
